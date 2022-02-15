9bbea3bcd4c50d139bd134a90fc92a4cc739c7c1
Anu and Sri Kemburu <avisfy0@hotmail.com>
2022-02-15 14:41:04 -0500

    2.40pm, working commit

--- app.js
+++ app.js
@@ -18,56 +18,14 @@
 			var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 			var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-            var myImg1 = document.querySelector('img1')
-            var myImg2 = document.querySelector('img2')
+            // var myImg1 = document.querySelector('#img1')
+            // var myImg2 = document.querySelector('#img2')
 
-            var path;
-            var ArrayImage = ["./images/dice1.png", "./images/dice2.png", "./images/dice3.png", "./images/dice4.png", "./images/dice5.png", "./images/dice6.png"];
-
-            // srikemburu.github.io-Game_Project\images\dice1.png
-            // C:\Users\kembu\srikemburu.github.io\srikemburu.github.io-Game_Project\images\dice2.png
-
-
-            path = ArrayImage[randomNumber1-1];
-            console.log (path)
-            myImg1.setAttribute("src",path);
-
-            path = ArrayImage[randomNumber2-1];
-            myImg2.setAttribute("src",path);
-
-            // myImg1.setAttribute("src",
-            // "images%2fdice" + randomNumber1 + ".png");
-            // myImg2.setAttribute("src",
-            // "images%2fdice" + randomNumber2 + ".png");
-
-        //    var rn1 = randomNumber1-1
-        //     switch (rn1){
-        //         case 0:
-        //             myImg1.setAttribute("src","images%2fdice1.png");       
-        //             break;
-        //         case 1:
-        //             myImg1.setAttribute("src","images%2fdice2.png"); 
-        //             break;
-        //         case 2:
-        //             myImg1.setAttribute("src","images%2fdice3.png");       
-        //             break;
-        //         case 3:
-        //             myImg1.setAttribute("src","images%2fdice4.png"); 
-        //             break;
-        //         case 4:
-        //             myImg1.setAttribute("src","images%2fdice5.png");       
-        //             break;
-        //         case 5:
-        //             myImg1.setAttribute("src","images%2fdice6.png"); 
-        //             break;
-        //     }
-
-
-			// document.querySelector(".img1").setAttribute("src",
-			// 	"images/dice" + randomNumber1 + ".png");
+			document.querySelector("#img1").setAttribute("src",
+				"images/dice" + randomNumber1 + ".png");
                 
-			// document.querySelector(".img2").setAttribute("src",
-			// 	"images/dice" + randomNumber2 + ".png");
+			document.querySelector("#img2").setAttribute("src",
+				"images/dice" + randomNumber2 + ".png");
 
 			if (randomNumber1 === randomNumber2) {
 				document.querySelector("h1").innerHTML = "Draw!";
@@ -82,6 +40,6 @@
 				document.querySelector("h1").innerHTML
 								= (player1 + " WINS!");
 			}
-		}, 2500);
+		}, 1000);
 	}
 
