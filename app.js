893fb22b081f9197c8c4f5b6f66458cced618a6e
Anu and Sri Kemburu <avisfy0@hotmail.com>
2022-02-16 13:05:35 -0500

    16Feb22 lunch time commit

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-
-	// Player name
+// Variables
 	var player1 = "Player 1";
 	var player2 = "Player 2";
+    var cnt1 = 0;
+    var cnt2 = 0;
 
 	// Function to change the player name
 	function editNames() {
@@ -14,6 +15,11 @@
 
 	// Function to roll the dice
 	function rollTheDice() {
+
+        let p1Stats = document.querySelector("#Player1Info")
+        let p2Stats = document.querySelector("#Player2Info")
+        let btnInput = document.querySelector(".btn")
+    
 		setTimeout(function () {
 			var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 			var randomNumber2 = Math.floor(Math.random() * 6) + 1;
@@ -23,9 +29,13 @@
 
 			document.querySelector("#img1").setAttribute("src",
 				"images/dice" + randomNumber1 + ".png");
+
+            p1Stats.innerHTML = `Wins: ${cnt1}`
                 
 			document.querySelector("#img2").setAttribute("src",
 				"images/dice" + randomNumber2 + ".png");
+
+            p2Stats.innerHTML = `Wins: ${cnt2}`
 
 			if (randomNumber1 === randomNumber2) {
 				document.querySelector("h1").innerHTML = "Draw!";
@@ -33,12 +43,33 @@
 
 			else if (randomNumber1 < randomNumber2) {
 				document.querySelector("h1").innerHTML
-								= (player2 + " WINS!");
+								= (player2 + " Wins!");
+                cnt2++;
+                p2Stats.innerHTML = `Wins: ${cnt2}`
+
+                if (cnt2 == 5){ 
+                    document.querySelector("h1").innerHTML
+								= (player2 + " is the WINNER!");
+                                 
+                cnt1 = 0;
+                cnt2 = 0;
+                     
+                }
 			}
 
 			else {
 				document.querySelector("h1").innerHTML
-								= (player1 + " WINS!");
+								= (player1 + " Wins!");
+                cnt1++;
+                p1Stats.innerHTML = `Wins: ${cnt1}`
+                if (cnt1 == 5){ 
+                    document.querySelector("h1").innerHTML
+								= (player1 + " is the WINNER!");
+                // p1Stats.innerHTML = `Wins: ${cnt1}`
+                cnt1 = 0;
+                cnt2 = 0;      
+                             
+                }
 			}
 		}, 1000);
 	}
