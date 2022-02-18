eb6bc02665f1e9c80e6e6fb16ea75b78afde5ccf
Anu and Sri Kemburu <avisfy0@hotmail.com>
2022-02-18 10:19:43 -0500

    commit for presentation

--- README.md
+++ README.md
@@ -12,8 +12,7 @@
 
 #### Dice game rules: Two players roll the dice and the dice with the highest number would win the round. If both the dice have the same number, it would be a draw. The rounds are continued until a player wins a total of 5 rounds to be declared a winner. Once a winner is declared, there will be an option to restart the game via a button prompt.
 
-Github Link [Dice Game](https://github.com/srikemburu/srikemburu.github.io-Game_Project/blob/main/index.html).
-
+Github Link [Dice Game](https://srikemburu.github.io/srikemburu.github.io-Game_Project/).
 
 ### Technologies used for Dice Game
 
@@ -26,4 +25,3 @@ Github Link [Dice Game](https://github.com/srikemburu/srikemburu.github.io-Game_
 Dice Game in which a player repeatedly rolls a dice until either a 1 is rolled or the player decides to "hold".
 
 If the player rolls a 1, they score nothing and it becomes the next player's turn. If the player rolls any other number, it is added to their turn total and the player's turn continues. If a player chooses to "hold", their turn total is added to their score, and it becomes the next player's turn. The first player to score 20 or more points wins.
-
--- app.js
+++ app.js
@@ -10,19 +10,14 @@
 	function editNames() {
 		player1 = prompt("Change Player1 name");
 		player2 = prompt("Change player2 name");
-
 		document.querySelector("p.Player1").innerHTML = player1;
 		document.querySelector("p.Player2").innerHTML = player2;
 	}
 
 	// Function to roll the dice
-	function rollTheDice() {
-        
+	function rollTheDice() {        
         let p1Stats = document.querySelector("#Player1Info")
         let p2Stats = document.querySelector("#Player2Info")
-        
-      //  let btnInput = document.querySelector(".btn")
-      //  const btn1 = document.getElementById("btn");
 
         if (newGame) {     // Reset styling for a new game
             cnt1 = 0;
@@ -43,9 +38,6 @@
 		    setTimeout(function () {
 			var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 			var randomNumber2 = Math.floor(Math.random() * 6) + 1;
-
-            var myImg1 = document.querySelector('#img1')
-            var myImg2 = document.querySelector('#img2')
 
 			document.querySelector("#img1").setAttribute("src",
 				"images/dice" + randomNumber1 + ".png");
@@ -71,8 +63,6 @@
                     document.querySelector("h1").style.color = "green";
                     document.querySelector("h1").innerHTML
 								= (player2 + " is the WINNER!");
-
-                    document.querySelector("button").setAttribute("class", "btn")
                     document.querySelector("button").innerText = ("Start New Game");
                     document.querySelector("button").style.background='Blue'; 
                     newGame = true;      // Start a New Game
